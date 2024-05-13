973513cd83b3e3c7d9d26743a7e93dd676b5e259
TAmirtharaj <amirtharajt@stringserve.com>
2024-05-13 17:31:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ app.post("/updateTime", async (request, response) => {
   );
 });
 
-app.get("/Amirtharaj", (req, res) => {
+app.get("/Arun", (req, res) => {
   mysql.query(
     `SELECT 
     x.room_name, 
