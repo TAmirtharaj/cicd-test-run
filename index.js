6ab5c06d930e58299dd510a1bf017549d040731f
TAmirtharaj <amirtharajt@stringserve.com>
2024-05-13 16:56:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ app.post("/updateTime", async (request, response) => {
   );
 });
 
-app.get("/GetRoomDataaaa", (req, res) => {
+app.get("/GetRoomDat", (req, res) => {
   mysql.query(
     `SELECT 
     x.room_name, 
